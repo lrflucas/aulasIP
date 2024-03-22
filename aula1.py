333a35f7f478506ac43abb3af59a7e2f0d545393
Lucas Rocha Ferreira <lucaslrf@academicos.com>
2024-03-22 16:49:54 -0300

    Aula 1 - soma e código de idade (if, else)

--- aula1.py
+++ aula1.py
@@ -9,8 +9,17 @@
 # Crie um programa em Python que solicite ao usuário dois números e armazene
 # esses dois números em duas variáveis distintas.
 
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
-soma = int((n1+n2))
-print(soma)
+# n1 = int(input("Digite um número: "))
+# n2 = int(input("Digite outro número: "))
+# soma = int((n1+n2))
+# print(soma)
+# %%
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Pode entrar na festa!")
+elif idade >= 16 and idade < 18:
+    print("É hora de voltar para sua casa")
+else:
+    print("Vou chamar seus pais.")
 # %%

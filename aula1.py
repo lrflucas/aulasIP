637eca184a5e9b12c0cb0517e104328188d4ea1c
Lucas Rocha Ferreira <lucaslrf@academicos.com>
2024-03-22 16:10:30 -0300

    Aula 1 - soma

---
+++ aula1.py
@@ -0,0 +1,16 @@
+#%%
+# ola = "Hello World"
+# print(ola)
+
+# %%
+# nome = input("Digite seu nome: ")
+
+#%%
+# Crie um programa em Python que solicite ao usuário dois números e armazene
+# esses dois números em duas variáveis distintas.
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+soma = int((n1+n2))
+print(soma)
+# %%

bbd229cdf7c82bc62dd29694f8ac46875e7ce415
Lucas Rocha Ferreira <lucaslrf@academicos.com>
2024-04-12 16:57:04 -0300

    aula 2 + exercícios (que alguns tao errados)

--- aula2/aula2.py
+++ aula2/aula2.py
@@ -26,3 +26,31 @@ for nome in nomes:
     print(f"Olá, {nome}!")
 
 # %%
+
+# Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente
+
+for numero in range(1, 11):
+    print(numero)
+
+#%%
+
+# Faça um programa que converta metros para centímetros
+
+numero = float(input("Digite um número em metros: "))
+
+for cm in numero:
+    print(cm*100)
+
+#%%
+
+# Solicite ao usuário um valor numérico, inteiro ou real, e escrever se é positivo ou negativo
+# (considere o valor zero como positivo)
+
+numero = float(input("Digite um número: "))
+
+while numero >= 0:
+    print("Positivo")
+else:
+    print("Negativo")
+    
+# %%

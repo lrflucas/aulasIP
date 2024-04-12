1e8100596442eb746b9db2bfcbac73570c7a7b0c
Lucas Rocha Ferreira <lucaslrf@academicos.com>
2024-04-12 16:19:09 -0300

    aula 2

---
+++ aula2/aula2.py
@@ -0,0 +1,28 @@
+#%%
+
+x = int()
+
+while x < 1000:
+    print(x)
+    x += 1
+
+#%%
+
+for numero in range(1, 11):
+    print(numero)
+
+#%%
+
+lista_itens = ["feijão", "arroz", "carne"]
+
+for item in lista_itens:
+    print("O item agora é: " + item) 
+
+# %%
+
+nomes = ["João", "Maria", "Pedro", "Ana"]
+
+for nome in nomes:
+    print(f"Olá, {nome}!")
+
+# %%

6a15beb690d5e9f433d845bb241b2fa357be67fc
Lucas <rochaferreira19@gmail.com>
2024-04-18 02:45:24 -0300

    Segunda questão da lista 2

--- lista2/questao2.py
+++ lista2/questao2.py
@@ -2,3 +2,12 @@
 
 # Solicite ao usuário um valor numérico, inteiro ou real, e escrever se é positivo ou negativo
 # (considere o valor zero como positivo).
+
+numero = float(input("Digite um número: "))
+
+if numero >= 0:
+    print("Positivo.")
+else:
+    print("Negativo.")
+    
+# %%

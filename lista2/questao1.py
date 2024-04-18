b00b6d665e9be2a614394be30e7ddf4ffa0ef9b6
Lucas <rochaferreira19@gmail.com>
2024-04-18 02:22:53 -0300

    Primeira questão da lista 2

--- lista2/questao1.py
+++ lista2/questao1.py
@@ -2,3 +2,14 @@
 
 # Solicite ao usuário um valor numérico, inteiro ou real, e verifique se ele é maior ou menor que 10. 
 # O programa deve escrever a mensagem correspondente (maior ou menor) e informar o valor digitado pelo usuário.
+
+numero = float(input("Digite um número: "))
+
+if numero > 10:
+    print(f"{numero} é maior que 10.")
+elif numero < 10:
+    print(f"{numero} é menor que 10.")
+else:
+    print(f"{numero} é igual a 10.")
+
+# %%

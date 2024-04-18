65e5c05da60f138e83e9f581718658b803051524
Lucas <rochaferreira19@gmail.com>
2024-04-18 01:46:04 -0300

    Questão finalizada

--- lista1/questao5.py
+++ lista1/questao5.py
@@ -2,3 +2,12 @@
 
 # Crie um programa em Python em que as quatro operações em que todos os resultados resultam em 8.
 
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro: "))
+
+print(f"{n1}+{n2} = 8")
+print(f"{n1}-{n2} = 8")
+print(f"{n1}*{n2} = 8")
+print(f"{n1}/{n2} = 8")
+
+# %%

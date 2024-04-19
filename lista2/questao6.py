6508a75039da3f926afb0ea40317a34f00395318
Lucas <rochaferreira19@gmail.com>
2024-04-19 01:26:34 -0300

    Sexta questão da lsita 2

--- lista2/questao6.py
+++ lista2/questao6.py
@@ -1,3 +1,13 @@
 #%%
 
 # Ler dois valores (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+if n1 > n2:
+    print(f"{n2}, {n1}")
+else:
+    print(f"{n1}, {n2}")
+
+# %%

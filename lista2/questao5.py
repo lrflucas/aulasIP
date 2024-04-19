503bff93c10655d805ede9beaa90b35369ef4280
Lucas <rochaferreira19@gmail.com>
2024-04-19 01:23:25 -0300

    Quinta questão da lista 2

--- lista2/questao5.py
+++ lista2/questao5.py
@@ -1,3 +1,13 @@
 #%%
 
 # Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+if n1 > n2:
+    print(f"{n1} é maior.")
+else:
+    print(f"{n2} é maior.")
+
+# %%

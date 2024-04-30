f43e5b27b2a38f319820ead34e8ae31f4796315e
Lucas <rochaferreira19@gmail.com>
2024-04-30 00:03:10 -0300

    Quinta questão da lista 3

--- lista3/questao5.py
+++ lista3/questao5.py
@@ -2,3 +2,10 @@
 
 # Utilizando a lista do exercício anterior, exiba uma saudação ("Olá como vai você"), personalizado com o nome de cada amigo.
 # A saudação deve ser a mesma, alterando apenas o nome do amigo.
+
+amigos = ["Cauã", "Pedro", "Rodrigo", "Filipe"]
+
+for nome in amigos:
+    print(f"Olá, {nome}, como vai você?")
+
+# %%

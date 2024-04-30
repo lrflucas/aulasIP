a67c7529409d04efe0e99a7149c503b314fb993c
Lucas <rochaferreira19@gmail.com>
2024-04-29 22:46:23 -0300

    Segunda questão da lista 3

--- lista3/questao2.py
+++ lista3/questao2.py
@@ -1,3 +1,12 @@
 #%%
 
 # Faça um programa que mostre as tabuadas dos números de 1 a 10.
+
+for numero in range(1, 11):
+  print(f"Essa é a tabuada do {numero}: ")
+
+  for outronum in range(1, 11):
+    tabuada = (numero * outronum)
+    print(f"{numero} x {outronum} = {tabuada}")
+
+# %%

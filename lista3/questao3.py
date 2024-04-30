a19fa8cd4501b2d112e969f945a37d69ebead9b4
Lucas <rochaferreira19@gmail.com>
2024-04-29 23:01:39 -0300

    Terceira questão da lista 3

--- lista3/questao3.py
+++ lista3/questao3.py
@@ -2,3 +2,10 @@
 
 # Armazene os nomes de alguns de seus amigos em uma lista chamada amigos.
 # Exiba o nome de cada pessoa acessando cada elemento da lista um de cada vez.
+
+amigos = ["Cauã", "Pedro", "Rodrigo", "Filipe"]
+
+for nome in amigos:
+    print(nome)
+
+# %%

7975277673eaff2260472f0c54f3f9622456c4a6
Lucas <rochaferreira19@gmail.com>
2024-04-29 23:35:31 -0300

    Quarta questão da lista 3

--- lista3/questao4.py
+++ lista3/questao4.py
@@ -1,3 +1,13 @@
 #%%
 
 # Faça um programa que receba um número e que calcule e mostre a tabuada desse número.
+
+numero = int(input("Digite um número pra descobrir a tabuada: "))
+
+print(f"Essa é a tabuada do {numero}: ")
+
+for outronum in range(1, 11):
+    tabuada = (numero * outronum)
+    print(f"{numero} x {outronum} = {tabuada}")
+    
+# %%

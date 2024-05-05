a2ed7168e15a59dda2c8c77fb43d1e7b44f23108
Lucas <rochaferreira19@gmail.com>
2024-05-05 16:38:07 -0300

    Sétima questão da lista 3

--- lista3/questao7.py
+++ lista3/questao7.py
@@ -1,3 +1,25 @@
 #%%
 
 # Faça um cadastro de usuários com nome, idade e email, utilizando apenas o que você aprendeu até agora.
+
+usuarios = []
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+email = input("Digite seu e-mail: ")
+
+usuario = {
+    "Nome": nome,
+    "Idade": idade,
+    "Email": email
+}
+
+usuarios.append(usuario) # append adiciona só uma vez
+
+for novousuario in usuarios:
+    print("Nome: ", usuario["Nome"])
+    print("Idade: ", usuario["Idade"])
+    print("E-mail: ", usuario["Email"])
+    print("\n")
+
+# %%

aa15c80044a172c0ee323bad9d1a99b163bb384f
Lucas <rochaferreira19@gmail.com>
2024-05-05 00:41:06 -0300

    Sexta questão da lista 3

--- lista3/questao6.py
+++ lista3/questao6.py
@@ -7,3 +7,24 @@
     #    Imprima o nome das pessoas que não poderão comparecer.
     # d. Modifique sua lista, substitua os desistentes por novos convidados.
     # e. Exiba um novo convite para cada pessoa que continua presente em sua lista.
+
+convidados = ["Cristiano Ronaldo", "LeBron James", "Jaden Smith", "Josh Dun", "Kendrick Lamar"]
+
+for nome in convidados:
+    print(f"Sr. {nome}, você está convidado para um jantar às 20h, no dia 30/04, na minha casa.")
+
+print("\n")
+
+convidados.remove("Cristiano Ronaldo")
+print("Não poderá comparecer: Cristiano Ronaldo")
+
+print(convidados)
+
+print("\n")
+
+convidados.insert(4, "21 Savage")
+
+for nome in convidados:
+    print(f"Sr. {nome}, devido ao cancelamento repentino do sr. Cristiano Ronaldo, estou reenviando o convitepara um jantar às 20h, no dia 30/04, na minha casa.")
+
+# %%

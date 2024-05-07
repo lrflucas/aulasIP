9f40ca54552b4d9a8d48f2f7458b56f84e91cfaf
Lucas Rocha Ferreira <lucaslrf@academicos.com>
2024-05-07 18:26:19 -0300

    primeira questão da lista 4

--- lista4/questao1.py
+++ lista4/questao1.py
@@ -1,1 +1,10 @@
+#%%
+
 # Faça um programa que converta metros para centímetros.
+
+metro = float(input("Digite um valor em metros para ser convertido para centímetros: "))
+cm = metro * 100
+
+print(f"O valor {metro} metros é {cm} centímetros.")
+
+# %%

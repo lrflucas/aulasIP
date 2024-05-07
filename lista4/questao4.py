fcbe904cb43c44eccc090211ebba9424cb7e3fff
Lucas Rocha Ferreira <lucaslrf@academicos.com>
2024-05-07 19:00:49 -0300

    quarta questão da lista 4 - incompleta

--- lista4/questao4.py
+++ lista4/questao4.py
@@ -1,2 +1,9 @@
 # Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 # Calcule e mostre o total do seu salário no referido mês.
+
+ganhohora = float(input("Digite quanto você ganha por hora: "))
+horas = int(input("Digite o número de horas trabalhadas no mês: "))
+meses = int(input("Digite quantos dias tem o mês que você quer saber o salário: "))
+
+if meses == 28:
+    print()

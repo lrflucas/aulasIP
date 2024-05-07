13f7287909a3a452d1b24c430d66a95a3fe22c53
Lucas Rocha Ferreira <lucaslrf@academicos.com>
2024-05-07 18:44:35 -0300

    terceira questão da lista 4

--- lista4/questao3.py
+++ lista4/questao3.py
@@ -1,1 +1,11 @@
+#%%
+
 # Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado = float(input("Digite o tamanho do lado do quadrado: "))
+area = lado ** 2
+dobro = area * 2
+
+print(f"A área do quadrado é {area} cm². O dobro dessa área é {dobro} cm².")
+
+# %%

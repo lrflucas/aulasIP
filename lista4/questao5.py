c7691b9f89cab9272cb6e52fe983317b4db5ae83
Lucas <rochaferreira19@gmail.com>
2024-05-10 01:02:41 -0300

    quinta questão da lista 4

--- lista4/questao5.py
+++ lista4/questao5.py
@@ -1,2 +1,11 @@
+#%%
+
 # Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
 #   1. C = 5 * ((F-32) / 9).
+
+temperatura = float(input("Digite a temperatura em graus Fahrenheit: "))
+celsius = 5 * ((temperatura-32) / 9)
+
+print(f"Temperatura em Fahrenheit é {temperatura}°F.\nTemperatura em Celsius é {celsius}°C.")
+
+# %%

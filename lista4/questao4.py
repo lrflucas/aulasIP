0a65b8f5f317d6150b68ef7f766f275b7a445f2f
Lucas <rochaferreira19@gmail.com>
2024-05-10 00:49:19 -0300

    quarta questão da lista 4

--- lista4/questao4.py
+++ lista4/questao4.py
@@ -1,3 +1,5 @@
+#%%
+
 # Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 # Calcule e mostre o total do seu salário no referido mês.
 
@@ -6,4 +8,14 @@ horas = int(input("Digite o número de horas trabalhadas no mês: "))
 meses = int(input("Digite quantos dias tem o mês que você quer saber o salário: "))
 
 if meses == 28:
-    print()
+    print(f"Você ganha R${ganhohora*horas} em um mês com {meses} dias.")
+elif meses == 29:
+    print(f"Você ganha R${ganhohora*horas} em um mês com {meses} dias.")
+elif meses == 30:
+    print(f"Você ganha R${ganhohora*horas} em um mês com {meses} dias.")
+elif meses == 31:
+    print(f"Você ganha R${ganhohora*horas} em um mês com {meses} dias.")
+else:
+    print("Impossível calcular, mês inválido.")
+    
+# %%

3546a5e61152e3225e6c76313cf65c330082c228
Lucas <rochaferreira19@gmail.com>
2024-05-16 01:17:13 -0300

    oitava questão da lista 4

--- lista4/questao8.py
+++ lista4/questao8.py
@@ -3,7 +3,7 @@
 # Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal,
 # usando a seguinte fórmula: (72.7*altura) - 58
 
-altura = float(input("Digite a sua altura em centímetros: "))
+altura = float(input("Digite a sua altura em metros: "))
 formula = (72.7 * altura) - 58
 
 print(f"Sua altura: {altura}")

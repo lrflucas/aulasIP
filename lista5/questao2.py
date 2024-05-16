1593c7b5f3d0421fb9803936e36d7acfdb5a9e9b
Lucas <rochaferreira19@gmail.com>
2024-05-16 01:31:15 -0300

    lista 5

---
+++ lista5/questao2.py
@@ -0,0 +1,3 @@
+# Use um dicionário para armazenar os números favoritos de algumas pessoas.
+# Escolha cinco colegas, e pergunte quais seus números favoritos.
+# Use seus nomes para serem as chaves de cada número favorito. Ao final, exiba o nome de cada pessoa e seu número favorito.

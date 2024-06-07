9a8403f27a359a12eaa6a159f539a607cf5e45ca
Lucas <rochaferreira19@gmail.com>
2024-06-07 17:08:42 -0300

    segunda questão da lista 5

--- lista5/questao2.py
+++ lista5/questao2.py
@@ -1,3 +1,12 @@
+#%%
+
 # Use um dicionário para armazenar os números favoritos de algumas pessoas.
 # Escolha cinco colegas, e pergunte quais seus números favoritos.
 # Use seus nomes para serem as chaves de cada número favorito. Ao final, exiba o nome de cada pessoa e seu número favorito.
+
+numeros = {"Pedro Brito": 28, "Cinthia": 23, "Rodrigo": 25, "Cauã": 9, "Filipe": 7}
+
+for nome, numero in numeros.items(): # pra acessar tanto as chaves quanto os valores, você usa items().
+    print(f"{nome}: {numero}")
+
+# %%

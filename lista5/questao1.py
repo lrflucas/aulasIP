722d38ce40e153a5c8e8ecab86f45297b31f4cde
Lucas <rochaferreira19@gmail.com>
2024-06-07 15:55:05 -0300

    primeira questão da lista 5

--- lista5/questao1.py
+++ lista5/questao1.py
@@ -1,4 +1,15 @@
+#%%
+
 # Use um dicionário para armazenar informações sobre uma pessoa que você conheça.
 # Armazene seu primeiro nome, o sobrenome, a idade e a cidade em que ela vive.
 # Você deverá ter chaves como primeiro_nome, sobrenome, idade, e cidade.
 # Por fim, mostre cada informação armazenada em seu dicionário.
+
+pessoa = {"primeiro_nome": "Gabriela", "sobrenome": "Rocha", "idade": 40, "cidade": "João Pessoa"}
+
+print(f"Primeiro nome: {pessoa["primeiro_nome"]}")
+print(f"Sobrenome: {pessoa["sobrenome"]}")
+print(f"Idade: {pessoa["idade"]}")
+print(f"Cidade: {pessoa["cidade"]}")
+
+# %%

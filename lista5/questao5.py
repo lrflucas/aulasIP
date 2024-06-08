115b91ca416c87ea8d2d3229f0681d11a93766fa
Lucas <rochaferreira19@gmail.com>
2024-06-07 23:32:19 -0300

    quinta questão da lista 5 - LISTA FINALIZADA

--- lista5/questao5.py
+++ lista5/questao5.py
@@ -1,1 +1,17 @@
+#%%
+
 # Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+
+#! BOA PARTE PEGUEI DO GITHUB
+
+numeros = []
+
+for i in range(5):
+    numero = int(input(f"Digite o {i+1}º número inteiro: "))
+    numeros.append(numero)
+
+print("Os números foram: ")
+for numero in numeros:
+   print(numero)
+
+# %%
